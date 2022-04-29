7e2b7418a4cac0e1ffedb24fae03dfc243d8140b
Albert "Christian" DeMarco <Kelter-1313>
2022-04-29 18:44:08 -0400

    formatting

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const fs = require('fs')
 
 const express = require('express')
 const app = express()
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // Serve static HTML files
@@ -21,52 +21,52 @@ const args = require("minimist")(process.argv.slice(2))
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
 
-if (args["help"] == null){
-const db = require('./src/services/database')
+if (args["help"] == null) {
+  const db = require('./src/services/database')
 
-if(args["log"] == null || args["log"] == "true"){
-  var logFlag = true
-}else{logFlag = false}
-// ^^ --log behavior 
+  if (args["log"] == null || args["log"] == "true") {
+    var logFlag = true
+  } else { logFlag = false }
+  // ^^ --log behavior 
 
 
-var port = args["port"]
-// Make this const default to port 3000 if there is no argument given for `--port`.
-if (port == null){
-  port = 5000
-}
-console.log(port)
-// Use the fs module to create an arrow function using `fs.readFile`.
-// Use the documentation for the Node.js `fs` module. 
-// The function must read a file located at `./www/index.html` and do some stuff with it.
-// The stuff that should be inside this function is all below.
-var server = app.listen(port, () => {
-  console.log(`App is running on port ${port}`)
-})
-
-app.use((req, res, next) => {
-  let logdata = {
-    remoteaddr: req.ip,
-    remoteuser: req.user,
-    time: Date.now(),
-    method: req.method,
-    url: req.url,
-    protocol: req.protocol,
-    httpversion: req.httpVersion,
-    status: res.statusCode,
-    referer: req.headers['referer'],
-    useragent: req.headers['user-agent']
+  var port = args["port"]
+  // Make this const default to port 3000 if there is no argument given for `--port`.
+  if (port == null) {
+    port = 5000
   }
-  if(args["log"] == "false"){
+  console.log(port)
+  // Use the fs module to create an arrow function using `fs.readFile`.
+  // Use the documentation for the Node.js `fs` module. 
+  // The function must read a file located at `./www/index.html` and do some stuff with it.
+  // The stuff that should be inside this function is all below.
+  var server = app.listen(port, () => {
+    console.log(`App is running on port ${port}`)
+  })
 
-  }else{
-    const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
-    // Set up the access logging middleware
-    app.use(
-      morgan('combined', { stream: accessLog })
+  app.use((req, res, next) => {
+    let logdata = {
+      remoteaddr: req.ip,
+      remoteuser: req.user,
+      time: Date.now(),
+      method: req.method,
+      url: req.url,
+      protocol: req.protocol,
+      httpversion: req.httpVersion,
+      status: res.statusCode,
+      referer: req.headers['referer'],
+      useragent: req.headers['user-agent']
+    }
+    if (args["log"] == "false") {
+
+    } else {
+      const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
+      // Set up the access logging middleware
+      app.use(
+        morgan('combined', { stream: accessLog })
       )
-  }
-  const stmt = db.prepare(`INSERT INTO accesslog (remoteaddr ,
+    }
+    const stmt = db.prepare(`INSERT INTO accesslog (remoteaddr ,
     remoteuser ,
     time ,
     method ,
@@ -77,136 +77,136 @@ app.use((req, res, next) => {
     referer ,
     useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
   `)
-  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
-  next()
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    next()
   })
 
-app.get("/app", (req, res, next) => {
-  res.json({"message":"Your API works! (200)"})
-  res.status(200)
-})
+  app.get("/app", (req, res, next) => {
+    res.json({ "message": "Your API works! (200)" })
+    res.status(200)
+  })
 
-if(args["debug"] == "true" || args["debug"] == null){
-  /* must create endpoints
-
-  --debug	If set to true, creates endlpoints /app/log/access/ which returns
-              	a JSON access log from the database and /app/error which throws 
-              	an error with the message "Error test successful." Defaults to 
-		false.
-
-*/
-  app.get("/app/log/access", (req, res, next) => {
+  if (args["debug"] == "true" || args["debug"] == null) {
+    /* must create endpoints
+  
+    --debug	If set to true, creates endlpoints /app/log/access/ which returns
+                  a JSON access log from the database and /app/error which throws 
+                  an error with the message "Error test successful." Defaults to 
+      false.
+  
+  */
+    app.get("/app/log/access", (req, res, next) => {
       const stmt = db.prepare('SELECT * FROM accesslog').all()
       res.status(200).json(stmt)
-})
-  app.get("/app/error", (req, res, next)=>{
-    throw new Error('Error test successful.')
+    })
+    app.get("/app/error", (req, res, next) => {
+      throw new Error('Error test successful.')
+    })
+
+
+  }
+
+
+  // Necessary Functions
+  function coinFlip() {
+    var a = Math.floor(Math.random() * 10) + 1;
+    if (a <= 5) {
+      return "heads";
+    }
+    else {
+      return "tails";
+    }
+  }
+
+  function flipACoin(call) {
+    var flipResult = coinFlip();
+    var returnDict = { "call": call, "flip": flipResult, "result": "" };
+    var resultOfCall = "lose";
+    if (call == flipResult) {
+      resultOfCall = "win";
+    }
+    returnDict["result"] = resultOfCall;
+    return returnDict;
+  }
+
+  function coinFlips(flips = 1) {
+    var a = [];
+    for (let x = 0; x < flips; x++) {
+      a.push(coinFlip());
+    }
+    return a;
+  }
+
+  function countFlips(array) {
+    var a = Array.from(array);
+    var counts = { "heads": 0, "tails": 0 }
+    for (let x = 0; x < array.length; x++) {
+      if (a.pop() == "heads") {
+        counts["heads"] += 1;
+      }
+      else {
+        counts["tails"] += 1;
+      }
+    }
+    return counts;
+  }
+
+  app.get('/app/flip/coins/', (req, res, next) => {
+    const flips = coinFlips(req.body.number)
+    const count = countFlips(flips)
+    res.type('text/json')
+    res.status(200).json({ "raw": flips, "summary": count })
   })
 
 
-}
+  app.get("/app/flip/", (req, res, next) => {
+    flip = coinFlip()
+    console.log(flip)
+    res.type('text/json')
+    res.status(200).json({ "flip": flip })
+  })
 
+  app.get('/app/flips/:number', (req, res, next) => {
+    const flips = coinFlips(req.body.number)
+    const count = countFlips(flips)
+    res.type('text/json')
+    res.status(200).json({ "raw": flips, "summary": count })
+  });
 
-// Necessary Functions
-function coinFlip() {
-  var a = Math.floor(Math.random() * 10) + 1;
-  if (a <= 5){
-    return "heads";
-  }
-  else{
-    return "tails";
-  }
-}
- 
-function flipACoin(call) {
-  var flipResult = coinFlip();
-  var returnDict = {"call": call, "flip": flipResult, "result": ""};
-  var resultOfCall = "lose";
-  if (call == flipResult){
-    resultOfCall = "win";
-  }
-  returnDict["result"] = resultOfCall;
-  return returnDict;
-}
- 
-function coinFlips(flips=1) {
-  var a = [];
-  for (let x = 0; x < flips; x++) {
-    a.push(coinFlip());
-  }
-  return a;
-}
- 
-function countFlips(array) {
-  var a = Array.from(array);
-  var counts = {"heads": 0, "tails": 0}
-  for (let x = 0; x < array.length; x++) {
-    if (a.pop() == "heads"){
-      counts["heads"]+=1;
-    }
-    else{
-      counts["tails"]+=1;
-    }
-  }
-  return counts;
-}
+  app.get('/app/flip/call/:guess(heads|tails)/', (req, res, next) => {
+    const game = flipACoin(req.body.guess)
+    res.status(200).json(game)
+  })
 
-app.get('/app/flip/coins/', (req, res, next) => {
-  const flips = coinFlips(req.body.number)
-  const count = countFlips(flips)
-  res.type('text/json')
-  res.status(200).json({"raw":flips,"summary":count})
-})
- 
- 
-app.get("/app/flip/", (req,res, next) => {
-  flip = coinFlip()
-  console.log(flip)
-  res.type('text/json')
-  res.status(200).json({"flip":flip})
-})
- 
-app.get('/app/flips/:number', (req, res, next) => {
-  const flips = coinFlips(req.body.number)
-  const count = countFlips(flips)
-  res.type('text/json')
-  res.status(200).json({"raw":flips,"summary":count})
-});
+  // Flip a bunch of coins with one body variable (number)
+  app.post('/app/flip/coins/', (req, res, next) => {
+    const flips = coinFlips(req.body.number)
+    const count = countFlips(flips)
+    res.status(200).json({ "raw": flips, "summary": count })
+  })
 
-app.get('/app/flip/call/:guess(heads|tails)/', (req, res, next) => {
-  const game = flipACoin(req.body.guess)
-  res.status(200).json(game)
-})
+  app.get("/app/flip/call/heads/", (req, res) => {
+    res.status(200).json(flipACoin("heads"))
+  })
 
-// Flip a bunch of coins with one body variable (number)
-app.post('/app/flip/coins/', (req, res, next) => {
-  const flips = coinFlips(req.body.number)
-  const count = countFlips(flips)
-  res.status(200).json({"raw":flips,"summary":count})
-})
+  app.get("/app/flip/call/tails/", (req, res) => {
+    res.status(200).json(flipACoin("tails"))
+  })
 
-app.get("/app/flip/call/heads/", (req,res) =>{
-  res.status(200).json(flipACoin("heads"))
-})
- 
-app.get("/app/flip/call/tails/", (req,res) =>{
-  res.status(200).json(flipACoin("tails"))
-})
+  app.post('/app/flip/call/', (req, res, next) => {
+    const game = flipACoin(req.body.guess)
+    res.status(200).json(game)
+  })
 
-app.post('/app/flip/call/', (req, res, next) => {
-  const game = flipACoin(req.body.guess)
-  res.status(200).json(game)
-})
-
-app.use(function(req, res, next){
-  res.json({"message":"Endpoint not found. (404)"})
-  res.status(404)
-})
+  app.use(function (req, res, next) {
+    res.json({ "message": "Endpoint not found. (404)" })
+    res.status(404)
+  })
 
 
 
-// case in which --help used
-}else{
+  // case in which --help used
+} else {
   console.log(`--port, -p	Set the port number for the server to listen on. Must be an integer
   between 1 and 65535. Defaults to 5000.
 
--- public/main.js
+++ public/main.js
@@ -2,219 +2,196 @@
 
 
 const homeButton = document.getElementById("homenav")
-			// Add event listener for coins form
-			homeButton.addEventListener("click", moveHome)
-			// Create the submit handler
-            
-			function moveHome() {
-                // go home
-                document.getElementById("home").className = "active"
-                document.getElementById("single").className = "hidden"
-                document.getElementById("multi").className = "hidden"
-                document.getElementById("guess").className = "hidden"
+// Add event listener for coins form
+homeButton.addEventListener("click", moveHome)
+// Create the submit handler
 
-            }
+function moveHome() {
+    // go home
+    document.getElementById("home").className = "active"
+    document.getElementById("single").className = "hidden"
+    document.getElementById("multi").className = "hidden"
+    document.getElementById("guess").className = "hidden"
+
+}
 
 const singleFlip = document.getElementById("singlenav")
-			// Add event listener for coins form
-			singleFlip.addEventListener("click", moveSingleFlip)
-			// Create the submit handler
-			function moveSingleFlip() {
-                // go to single flip
-                document.getElementById("home").className = "hidden"
-                document.getElementById("single").className = "active"
-                document.getElementById("multi").className = "hidden"
-                document.getElementById("guess").className = "hidden"
-            }
+// Add event listener for coins form
+singleFlip.addEventListener("click", moveSingleFlip)
+// Create the submit handler
+function moveSingleFlip() {
+    // go to single flip
+    document.getElementById("home").className = "hidden"
+    document.getElementById("single").className = "active"
+    document.getElementById("multi").className = "hidden"
+    document.getElementById("guess").className = "hidden"
+}
 
 const multiFlip = document.getElementById("multinav")
-			// Add event listener for coins form
-			multiFlip.addEventListener("click", moveMultiFlip)
-			// Create the submit handler
-			function moveMultiFlip() {
-                // go to multi flip
-                document.getElementById("home").className = "hidden";
-                document.getElementById("single").className = "hidden";
-                document.getElementById("multi").className = "active";
-                document.getElementById("guess").className = "hidden";
-            }
+// Add event listener for coins form
+multiFlip.addEventListener("click", moveMultiFlip)
+// Create the submit handler
+function moveMultiFlip() {
+    // go to multi flip
+    document.getElementById("home").className = "hidden";
+    document.getElementById("single").className = "hidden";
+    document.getElementById("multi").className = "active";
+    document.getElementById("guess").className = "hidden";
+}
 
 const guessFlip = document.getElementById("guessnav")
-			// Add event listener for coins form
-			guessFlip.addEventListener("click", moveGuess)
-			// Create the submit handler
-			function moveGuess() {
-                // go to multi flip
-                document.getElementById("home").className = "hidden";
-                document.getElementById("single").className = "hidden";
-                document.getElementById("multi").className = "hidden";
-                document.getElementById("guess").className = "active";
-            }
+// Add event listener for coins form
+guessFlip.addEventListener("click", moveGuess)
+// Create the submit handler
+function moveGuess() {
+    // go to multi flip
+    document.getElementById("home").className = "hidden";
+    document.getElementById("single").className = "hidden";
+    document.getElementById("multi").className = "hidden";
+    document.getElementById("guess").className = "active";
+}
 
 // Flip one coin and show coin image to match result when button clicked
 
 const singleFlipButton = document.getElementById("singleFlipCoinButton")
 
-			// Add event listener for coins form
-			singleFlipButton.addEventListener("click", flipSingle)
-			// Create the submit handler
+// Add event listener for coins form
+singleFlipButton.addEventListener("click", flipSingle)
+// Create the submit handler
 
-			async function flipSingle(){
-                    const endpoint = "app/flip/";
-                    const url = document.baseURI+endpoint;
-                    await fetch(url)
-                      .then(function(response) {
-                      return response.json();
-                      })
-                    .then(function(result) {
-                        console.log(result)
-                        document.getElementById("singleCoinImages").setAttribute("src", "./assets/img/"+result.flip+".png");
-                        }
-                    )
-            }
-            
-                    
+async function flipSingle() {
+    const endpoint = "app/flip/";
+    const url = document.baseURI + endpoint;
+    await fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result)
+            document.getElementById("singleCoinImages").setAttribute("src", "./assets/img/" + result.flip + ".png");
+        }
+        )
+}
 
-                
+
+
+
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
 const multiFlipButton = document.getElementById("multiFlipForm")
-			// Add event listener for coins form
-			multiFlipButton.addEventListener("submit", flipMulti)
-			// Create the submit handler
-			async function flipMulti(event){
-                event.preventDefault();
-                a = document.getElementById("number").value // doesn't seem to pick up when something is inputted
-                document.getElementById("numberInputtedCheck").innerHTML = a;
-                const endpoint = "app/flip/coins/";
-                const url = document.baseURI+endpoint;
-                      const formEvent = event.currentTarget
-                      try {
-                        const formData = new FormData(formEvent);
-                // Hand the form data off to the function that is actually going to interact with the API.
-                        const flips = await sendFlips({ url, formData });
-                // Process the response and manipulate some elements in div#multi.
-                        console.log(flips);
-                // Present the summary information.
-                        document.getElementById("headCount").innerHTML = "Heads: "+flips.summary.heads;
-                        document.getElementById("tailCount").innerHTML = "Tails: "+flips.summary.tails;
-                // This calls a function what will make a list of coin images based on the array of coin flip results.
-                // See below for coinList() function.
-                    document.getElementById("coinImages").innerHTML = coinList(flips.raw);
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }
-
-                // Create a data sender to sent POST request objects from FormData to send to the API using fetch()
-async function sendFlips({ url, formData }) {
-    // Extract the form data from the FormData object
-        const plainFormData = Object.fromEntries(formData.entries());
-    // Turn the FormData into JSON
-        const formDataJson = JSON.stringify(plainFormData);
-    // Show the console what is going to be sent in the API message body
-        console.log(formDataJson);
-    // Set up the request object for fetch()
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json"
-            },
-            body: formDataJson
-        };
-    // Send the request and wait for the response
-        const response = await fetch(url, options);
-    // Pass the response back to the event handler
-        return response.json()
+// Add event listener for coins form
+multiFlipButton.addEventListener("submit", flipMulti)
+// Create the submit handler
+async function flipMulti(event) {
+    event.preventDefault();
+    a = document.getElementById("number").value
+    document.getElementById("numberInputtedCheck").innerHTML = a;
+    const endpoint = "app/flip/coins/";
+    const url = document.baseURI + endpoint;
+    const formEvent = event.currentTarget
+    try {
+        const formData = new FormData(formEvent);
+        const flips = await sendFlips({ url, formData });
+        console.log(flips);
+        document.getElementById("headCount").innerHTML = "Heads: " + flips.summary.heads;
+        document.getElementById("tailCount").innerHTML = "Tails: " + flips.summary.tails;
+        document.getElementById("coinImages").innerHTML = coinList(flips.raw);
+    } catch (error) {
+        console.log(error);
     }
-            
-                function coinList(array) {
-                    let text = "";
-                    let arrayLength = array.length
-                    for (let i = 0; i < arrayLength; i++) {
-                      text += '<li><img src="assets/img/'+array[i]+'.png" class="bigcoin"></li>';
-                    }
-                    return text
-                  }
-            
+}
 
-            async function flipCoins(event) {
-                // Because we are using an event, we need to remove the default browser event, which is a reload.
-                    event.preventDefault();
-                // Build up the endpoint URL
-                    const endpoint = "app/flip/coins/"
-                    const url = document.baseURI+endpoint
-                // This extracts the data object from the form so we can run it through the FormData API
-                    const formEvent = event.currentTarget
-                // Give the data to FormData and wait for a response or log an error to console.
-                    try {
-                        const formData = new FormData(formEvent);
-                // Hand the form data off to the function that is actually going to interact with the API.
-                        const flips = await sendFlips({ url, formData });
-                // Process the response and manipulate some elements in div#multi.
-                        console.log(flips);
-                // Present the summary information.
-                        document.getElementById("heads").innerHTML = "Heads: "+flips.summary.heads;
-                        document.getElementById("tails").innerHTML = "Tails: "+flips.summary.tails;
-                // This calls a function what will make a list of coin images based on the array of coin flip results.
-                // See below for coinList() function.
-                    document.getElementById("coinlist").innerHTML = coinList(flips.raw);
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }
+async function sendFlips({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJson = JSON.stringify(plainFormData);
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: formDataJson
+    };
+    const response = await fetch(url, options);
+    return response.json()
+}
+
+function coinList(array) {
+    let text = "";
+    let arrayLength = array.length
+    for (let i = 0; i < arrayLength; i++) {
+        text += '<li><img src="assets/img/' + array[i] + '.png" class="bigcoin"></li>';
+    }
+    return text
+}
+
+
+async function flipCoins(event) {
+    event.preventDefault();
+    const endpoint = "app/flip/coins/"
+    const url = document.baseURI + endpoint
+    const formEvent = event.currentTarget
+    try {
+        const formData = new FormData(formEvent);
+        const flips = await sendFlips({ url, formData });
+        document.getElementById("heads").innerHTML = "Heads: " + flips.summary.heads;
+        document.getElementById("tails").innerHTML = "Tails: " + flips.summary.tails;
+        document.getElementById("coinlist").innerHTML = coinList(flips.raw);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
 
 // Guess a flip by clicking either heads or tails button
 
 const guessHeadFlipButton = document.getElementById("guessHeadFlipCoinButton")
-			// Add event listener for coins form
-			guessHeadFlipButton.addEventListener("click", flipGuessHead)
-			// Create the submit handler
+// Add event listener for coins form
+guessHeadFlipButton.addEventListener("click", flipGuessHead)
+// Create the submit handler
 
-			async function flipGuessHead(){
-                    const endpoint = "app/flip/call/heads/";
-                    const url = document.baseURI+endpoint;
-                    await fetch(url)
-                      .then(function(response) {
-                      return response.json();
-                      })
-                    .then(function(result) {
-                        console.log(result)
-                        document.getElementById("guessCoinImages").setAttribute("src", "./assets/img/"+result.flip+".png");
-                        if(result.flip == "tails"){
-                            document.getElementById("winOrLose").innerHTML = "You guessed wrong... Oof...";
-                        }else{
-                            document.getElementById("winOrLose").innerHTML = "You guessed right! Congrats!";
-                        }
-                        }
-                    )
+async function flipGuessHead() {
+    const endpoint = "app/flip/call/heads/";
+    const url = document.baseURI + endpoint;
+    await fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result)
+            document.getElementById("guessCoinImages").setAttribute("src", "./assets/img/" + result.flip + ".png");
+            if (result.flip == "tails") {
+                document.getElementById("winOrLose").innerHTML = "You guessed wrong... Oof...";
+            } else {
+                document.getElementById("winOrLose").innerHTML = "You guessed right! Congrats!";
             }
+        }
+        )
+}
 
-            const guessTailsFlipButton = document.getElementById("guessTailFlipCoinButton")
-			// Add event listener for coins form
-			guessTailsFlipButton.addEventListener("click", flipGuessTail)
-			// Create the submit handler
+const guessTailsFlipButton = document.getElementById("guessTailFlipCoinButton")
+// Add event listener for coins form
+guessTailsFlipButton.addEventListener("click", flipGuessTail)
+// Create the submit handler
 
-			async function flipGuessTail(){
-                    const endpoint = "app/flip/call/tails/";
-                    const url = document.baseURI+endpoint;
-                    await fetch(url)
-                      .then(function(response) {
-                      return response.json();
-                      })
-                    .then(function(result) {
-                        console.log(result)
-                        document.getElementById("guessCoinImages").setAttribute("src", "./assets/img/"+result.flip+".png");
-                        if(result.flip == "tails"){
-                            document.getElementById("winOrLose").innerHTML = "You guessed right! Congrats!";
-                        }else{
-                            document.getElementById("winOrLose").innerHTML = "You guessed wrong... Oof...";
-                        }
-                        }
-                    )
+async function flipGuessTail() {
+    const endpoint = "app/flip/call/tails/";
+    const url = document.baseURI + endpoint;
+    await fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result)
+            document.getElementById("guessCoinImages").setAttribute("src", "./assets/img/" + result.flip + ".png");
+            if (result.flip == "tails") {
+                document.getElementById("winOrLose").innerHTML = "You guessed right! Congrats!";
+            } else {
+                document.getElementById("winOrLose").innerHTML = "You guessed wrong... Oof...";
             }
+        }
+        )
+}
